f0f1abe63c1cf02afc01c8248402218006588d23
Farbod T. Motlagh <farbod.motlagh@gmail.com>
2015-08-22 19:12:30 +0930

    plotting

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var mongoskin = require('mongoskin');
+var db = mongoskin.db('mongodb://root@localhost:27017/hashtag');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  fs.readFile('./hashtag.json', function(err, data){
-    res.render('index', { title: 'Hashtag', hashtag: data});
+  res.render('index', { title: 'Hashtag'});
+});
+
+router.get('/json/:hashtag', function(req, res, next) {
+  var hashtag =  req.params.hashtag;
+  db.collection('hashtags').findOne({ text: hashtag }, function(err, result) {
+    if (result) {
+      res.json(result);
+    } else {
+      var err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+    }
   });
 });
 
--- scripts/analyser.js
+++ scripts/analyser.js
@@ -1,52 +1,74 @@
+var fs = require('fs');
+
 var db = require('mongoskin').db('mongodb://root@localhost:27017/hashtag');
 var tweets = db.collection('tweets');
+var ht = db.collection('hashtags');
 
-var fs = require('fs');
 
 var hts;
 
 tweets.count(function(err, count) {
   var i = 0, counter = count;
   var hashtags = {}; 
+  var date;
   var perRequest = 100000;
   while (i < count) {
-    tweets.find({},{hashtags: 1}).skip(i).limit(perRequest).toArray(function(err, result) {
+    tweets.find({},{ hashtags: 1, created_at: 1}).skip(i).limit(perRequest).toArray(function(err, result) {
       console.log(counter);
       counter -= perRequest;
       result.forEach(function(tweet) {
+	date = new Date(tweet.created_at);
+	date = date.toDateString();
 	tweet.hashtags.forEach(function(hashtag) {
 	  hashtag = hashtag.toLowerCase();
-	  hashtags[hashtag] = hashtags[hashtag] || 0;
-	  hashtags[hashtag]++;
+	  hashtags[hashtag] = hashtags[hashtag] || {};
+	  hashtags[hashtag][date] = hashtags[hashtag][date] || 0;
+	  hashtags[hashtag].total = hashtags[hashtag].total || 0;
+	  hashtags[hashtag][date]++;
+	  hashtags[hashtag].total++;
 	});
       });
       result = null;
       if (counter < 0) {
-	console.log('It is ready');
-	process(hashtags);
+	console.log('Processing Now...');
+	store(hashtags);
       }
     });
     i += perRequest;
   }
 });
 
-function process(hashtags) {
+function store(hashtags) {
   var sortedHashtags = [];
   for(var hashtag in hashtags) {
-    if (hashtags[hashtag] > 1000) {
+    if (hashtags[hashtag].total > 200) {
       var h = {};
       h.text = hashtag;
-      h.count = hashtags[hashtag];
+      h.temporal = hashtags[hashtag];
+      h.total = hashtags[hashtag].total;
+      delete hashtags[hashtag].total;
       sortedHashtags.push(h);
     }
   }
   sortedHashtags.sort(function(a,b) {
-    return b.count - a.count;
+    return b.total - a.total;
   });
   hts = sortedHashtags;
-  fs.writeFile('./test.json', JSON.stringify(hts,null,4), function() {
-    console.log(hts);
+
+  var counter = hts.length;
+
+  ht.remove({}, function(err, result) {
+    hts.forEach(function(hashtag) {
+      ht.insert(hashtag, function(err, result) {
+	counter--;
+	if (counter == 0) {
+	  console.log('Done');
+	  process.exit(0);
+	}
+      });
+    });
   });
+
 }
 
 
