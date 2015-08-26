f82b9afb441d7dcd39cfc388c0898e39fffda07d
Farbod T. Motlagh <farbod.motlagh@gmail.com>
2015-08-26 20:46:09 +0930

    compare feature

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,52 @@
 $('#form-hashtag').submit(function(e) {
   e.preventDefault();
-  var hashtag = $('#input-hashtag').val();
-  $.getJSON('/json/' + hashtag, function(data) {
-    if (data) {
-      var labels = getDates(new Date('19 Aug 2015'), new Date('25 Aug 2015'));
-      chartData.labels = labels;
-      chartData.datasets[0].data = []
-      labels.forEach(function(label) {
-        if (data.temporal[label]) {
-          chartData.datasets[0].data.push(data.temporal[label]);
-        } else {
-          chartData.datasets[0].data.push(0);
+  var hashtag1 = $('#input-hashtag-1').val();
+  var hashtag2 = $('#input-hashtag-2').val();
+  var hashtags = [hashtag1, hashtag2];
+
+  var labels = getDates(new Date('19 Aug 2015'), new Date('4 Sep 2015'));
+  chartData.labels = labels;
+  var count = 0;
+  hashtags.forEach(function(hashtag, i) {
+    chartData.datasets[i].data = []
+    $.getJSON('/json/' + hashtag).then(function(data) {
+      if (data) {
+        labels.forEach(function(label) {
+          if (data.temporal[label]) {
+            chartData.datasets[i].data.push(data.temporal[label]);
+          } else {
+            chartData.datasets[i].data.push(0);
+          }
+        });
+        count++;
+        if (count == hashtags.length) {
+          plot(chartData);
         }
-      });
-      // create a canvas a plot
-      canvas = document.createElement('canvas');
-      canvas.id = 'chart';
-      canvas.width = 1200;
-      canvas.height = 400;
-      document.getElementById('chart-container').innerHTML = '';
-      document.getElementById('chart-container').appendChild(canvas);
-      ctx = canvas.getContext('2d');
-      chart = new Chart(ctx).Line(chartData, {
-        bezierCurve: true
-      });
-    }
+      }
+    }).fail(function(){
+      count++;
+      if (count == hashtags.length) {
+        plot(chartData);
+      }
+    });
   });
+
   return false;
 });
+
+function plot(cData) {
+  // create a canvas a plot
+  canvas = document.createElement('canvas');
+  canvas.id = 'chart';
+  canvas.width = 1200;
+  canvas.height = 400;
+  document.getElementById('chart-container').innerHTML = '';
+  document.getElementById('chart-container').appendChild(canvas);
+  ctx = canvas.getContext('2d');
+  chart = new Chart(ctx).Line(cData, {
+    bezierCurve: true
+  });
+}
 
 function getDates(startDate, stopDate) {
     var dateArray = [];
@@ -42,18 +61,29 @@ function getDates(startDate, stopDate) {
 var canvas;
 var ctx;
 var chartData = {
-    labels: [],
-    datasets: [
-        {
-            label: "#",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: []
-        }
-    ]
+  labels: [],
+  datasets: [
+    {
+      label: "#",
+      fillColor: "rgba(151,187,205,0.2)",
+      strokeColor: "rgba(151,187,205,1)",
+      pointColor: "rgba(151,187,205,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(151,187,205,1)",
+      data: []
+    },
+    {
+      label: "#",
+      fillColor: "rgba(220,220,220,0.2)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+      data: []
+    }
+
+  ]
 };
 var chart;
