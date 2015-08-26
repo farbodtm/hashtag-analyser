ccd9a9cb1a0a07c2bd7e0ce6c13517ce727e917f
Farbod T. Motlagh <farbod.motlagh@gmail.com>
2015-08-27 00:49:40 +0930

    new aggregator

--- scripts/aggregator.js
+++ scripts/aggregator.js
@@ -10,50 +10,7 @@ var ht = db.collection('hashtags');
 
 var hts;
 
-tweets.count(function(err, count) {
-  console.log(chalk.yellow('Loading...'));
-  var i = 0, counter = 0;
-  var hashtags = {}; 
-  var date;
-  var perRequest = 100000;
-
-  var bar = new progress(chalk.yellow('Counting [:bar] :percent :etas'), {
-    complete: '=',
-    incomplete: ' ',
-    width: 50,
-    total: count
-  });
-
-  while (i < count) {
-    tweets.find({},{ hashtags: 1, created_at: 1}).skip(i).limit(perRequest).toArray(function(err, result) {
-      counter += perRequest;
-      bar.tick(perRequest);
-      result.forEach(function(tweet) {
-	// set the date
-	date = new Date(tweet.created_at);
-	date = moment(date).format('D MMM YYYY');
-	// counting each hastag of a tweet
-	tweet.hashtags.forEach(function(hashtag) {
-	  hashtag = hashtag.toLowerCase();
-	  hashtags[hashtag] = hashtags[hashtag] || {};
-	  hashtags[hashtag][date] = hashtags[hashtag][date] || 0;
-	  hashtags[hashtag].total = hashtags[hashtag].total || 0;
-	  hashtags[hashtag][date]++;
-	  hashtags[hashtag].total++;
-	});
-      });
-      result = null;
-      if (counter > count) {
-	console.log(chalk.yellow('Saving...'));
-	store(hashtags);
-      }
-    });
-    i += perRequest;
-  }
-
-});
-
-function store(hashtags) {
+var store = function(hashtags) {
   var sortedHashtags = [];
   for(var hashtag in hashtags) {
     if (hashtags[hashtag].total > 200) {
@@ -84,5 +41,49 @@ function store(hashtags) {
     });
   });
 }
+
+tweets.count(function(err, count) {
+  console.log(chalk.yellow('Loading...'));
+  var hashtags = {}; 
+  var date;
+  var perRequest = 100000;
+
+  var bar = new progress(chalk.yellow('Counting [:bar] :percent :etas'), {
+    complete: '=',
+    incomplete: ' ',
+    width: 50,
+    total: count
+  });
+
+  var process = function(i) {
+    tweets.find({},{ hashtags: 1, created_at: 1}).skip(i).limit(perRequest).toArray(function(err, result) {
+      bar.tick(perRequest);
+      result.forEach(function(tweet) {
+	// set the date
+	date = new Date(tweet.created_at);
+	date = moment(date).format('D MMM YYYY');
+	// counting each hastag of a tweet
+	tweet.hashtags.forEach(function(hashtag) {
+	  hashtag = hashtag.toLowerCase();
+	  hashtags[hashtag] = hashtags[hashtag] || {};
+	  hashtags[hashtag][date] = hashtags[hashtag][date] || 0;
+	  hashtags[hashtag].total = hashtags[hashtag].total || 0;
+	  hashtags[hashtag][date]++;
+	  hashtags[hashtag].total++;
+	});
+      });
+      result = null;
+      if (i > count) {
+	console.log(chalk.yellow('Saving...'));
+	store(hashtags);
+      } else {
+	process(i + perRequest);
+      }
+    });
+  }
+  process(0);
+
+});
+
 
 
